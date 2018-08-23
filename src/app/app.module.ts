c86690c127e3297e9e0bb7231ee9df1a946765cd
esaniello <shaniel.samadhan.natin@gmail.com>
2018-08-23 14:35:19 -0300

    updated register page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { RegisterPage } from '../pages/register/register';
-import { LoginPage } from '../pages/login/login';
-import { LoggedinPage } from '../pages/loggedin/loggedin';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { RegisterPage } from "../pages/register/register";
+import { LoginPage } from "../pages/login/login";
+import { LoggedinPage } from "../pages/loggedin/loggedin";
+import { ForgotPasswordPage } from "../pages/forgot-password/forgot-password";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 const firebaseAuth = {
   apiKey: "AIzaSyDUdPV5_oH8u1qhjySPUvDmjHLxPv11hOE",
@@ -22,14 +22,14 @@ const firebaseAuth = {
   messagingSenderId: "104740449037"
 };
 
-
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     RegisterPage,
     LoginPage,
-    LoggedinPage
+    LoggedinPage,
+    ForgotPasswordPage
   ],
   imports: [
     BrowserModule,
@@ -43,12 +43,13 @@ const firebaseAuth = {
     HomePage,
     RegisterPage,
     LoginPage,
-    LoggedinPage
+    LoggedinPage,
+    ForgotPasswordPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,13 @@
-import { Component, ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { LoggedinPage } from '../loggedin/loggedin';
+import { Component, ViewChild } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController
+} from "ionic-angular";
+import { AngularFireAuth } from "angularfire2/auth";
+import { LoggedinPage } from "../loggedin/loggedin";
+import { ForgotPasswordPage } from "../forgot-password/forgot-password";
 
 /**
  * Generated class for the LoginPage page.
@@ -12,41 +18,52 @@ import { LoggedinPage } from '../loggedin/loggedin';
 
 @IonicPage()
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
+  @ViewChild("email")
+  email;
+  @ViewChild("password")
+  pw;
 
-  @ViewChild('email') email;
-  @ViewChild('password') pw;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams , private fire:AngularFireAuth, public alertCtrl:AlertController) {
-  }
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private fire: AngularFireAuth,
+    public alertCtrl: AlertController
+  ) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+    console.log("ionViewDidLoad LoginPage");
   }
 
-  alert(message:string){
-    this.alertCtrl.create({
-      title:"Info!",
-      subTitle:message,
-      buttons:["OK"]
-    }).present();
+  alert(message: string) {
+    this.alertCtrl
+      .create({
+        title: "Info!",
+        subTitle: message,
+        buttons: ["OK"]
+      })
+      .present();
   }
 
-  logIn(){
-    this.fire.auth.signInWithEmailAndPassword(this.email.value , this.pw.value)
-    .then(data => {
-      console.log('got some data ' , data);
-      this.navCtrl.setRoot( LoggedinPage);
-      this.alert("Succes! You are logged in!");
-    })
-    .catch(error => {
-      this.alert(error.message);
-      console.log("There was an error " , error);
-    });
-    console.log(this.email.value , this.pw.value);
+  logIn() {
+    this.fire.auth
+      .signInWithEmailAndPassword(this.email.value, this.pw.value)
+      .then(data => {
+        console.log("got some data ", data);
+        this.navCtrl.setRoot(LoggedinPage);
+        this.alert("Succes! You are logged in!");
+      })
+      .catch(error => {
+        this.alert(error.message);
+        console.log("There was an error ", error);
+      });
+    console.log(this.email.value, this.pw.value);
   }
 
+  forgotPassword() {
+    this.navCtrl.push(ForgotPasswordPage);
+  }
 }
